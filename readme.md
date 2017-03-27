68fbcc01f468d0f309c57be321344c5b70e3a7c3
Giel Berkers <giel@happy-online.nl>
2017-03-27 10:45:24 +0200

    Updated readme

--- readme.md
+++ readme.md
@@ -1,2 +1,30 @@
 # CLI ProgressBar
 
+A basic progress bar for usage in CLI scripts. It provides the following information:
+
+- Progress Bar
+- Percentage
+- Current Iteration / Total Iterations
+- Estimated Time
+- Memory Usage
+
+---
+
+## Example
+
+The output will look somewhat like this:
+
+![example](examples/example.gif)
+
+## Installation
+
+Installation is done simply using Composer:
+
+    composer require kanduvisla/cli-progressbar
+
+## Usage
+
+    use Kanduvisla\ProgressBar;
+    ProgressBar::show($currentIteration, $totalIterations);
+    
+See `examples/basic.php` for a simple example.
--- src/Kanduvisla/ProgressBar.php
+++ src/Kanduvisla/ProgressBar.php
@@ -33,8 +33,8 @@ class ProgressBar
             self::$startTime = time();
         }
 
-        $p2 = floor($p / 2);
-        $progress = str_pad('', $p2, '=') . str_pad('', 50 - $p2);
+        $p2 = floor($p / 5);
+        $progress = str_pad('', $p2, '=') . str_pad('', 20 - $p2);
         $usage = round(memory_get_usage() / 1024, 1);
         /// Show progressbar:
         echo "Progress: \033[33m[" . $progress . "]\033[0m \033[1;36m" . number_format($p, 2) . "%\033[0m - ";
@@ -44,11 +44,11 @@ class ProgressBar
         }
         // Show ETA:
         $elapsedSeconds = time() - self::$startTime;
-        $etaSeconds = (1 - $percent) * ($elapsedSeconds / $percent);
+        @$etaSeconds = (1 - $percent) * ($elapsedSeconds / $percent);
         echo "\033[36mETA: " . @date('H:i:s', $etaSeconds) . "\033[0m - ";
-        
+
         // Show memory usage:
-        echo "\033[34mMEM: " . $usage . "k\033[0m  \r";
+        echo "\033[34m " . $usage . "k\033[0m  \r";
         if ($percent == 1) {
             unset(self::$startTime);
             echo "\n";
